e2a9d5d9bc24694e96fbc895e297076086bb1653
habiba.qadir <habiba.qadir@gizasystems.onmicrosoft.com>
2025-11-25 17:00:03 +0200

    test ci

--- lib/home_page_screen.dart
+++ lib/home_page_screen.dart
@@ -24,7 +24,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
       appBar: AppBar(
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
